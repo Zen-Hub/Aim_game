f26bc7b66db1fc3bcf6981ce2bfb4d8a67cac2ed
Zen-Hub <inboxshow1@gmail.com>
2021-08-18 15:24:49 +0300

    added all elements in js

---
+++ main.js
@@ -0,0 +1,8 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timEl = document.querySelector('#time')
+const board = document.querySelector('#board')
+const colors = ['Aqua', 'Fuchsia', 'Gold', 'OrangeRed', 'Fuchsia', 'Lime', 'RosyBrown', 'SteelBlue']
+let time = 0
+let score = 0
